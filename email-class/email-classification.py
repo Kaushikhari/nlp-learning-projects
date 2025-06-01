e7cea47fe89acaef43190a2cbc6735f95a2210b6
Kaushik H <kaushikhari824@gmail.com>
2025-06-01 20:45:24 +0530

    first_commit

---
+++ email-class/email-classification.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import spacy
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+nlp = spacy.load('en_core_web_sm-3.8.0-py3-none-any.whl')
+df = pd.read_csv('spam.csv')
+
+
+def preprocess(text):
+
+    doc = nlp(text)
+    filtered_tokens = []
+    for token in doc:
+        if token.is_stop or token.is_punct:
+            continue
+        filtered_tokens.append(token.lemma_)
+
+    return " ".join(filtered_tokens)
+
+
+df['preprocessed'] = df['Message'].apply(preprocess)
+df['spam'] = df['Category'].apply(lambda x: 1 if x == 'spam' else 0)
+
+X_train, X_test, y_train, y_test = train_test_split(
+                df.preprocessed,
+                df.spam,
+                test_size=0.2,
+                stratify=df.spam,
+                random_state=42
+)
+
+clf = Pipeline([
+    ('vectorizer', CountVectorizer(lowercase=True)),
+    ('nb', MultinomialNB())
+])
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=['Ham', 'Spam'], yticklabels=['Ham', 'Spam'])
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.show()
---
+++ nlp-foundations/02_info_extractor/info-extractor.py
@@ -0,0 +1,23 @@
+import spacy
+import regex
+nlp = spacy.load('en_core_web_sm')
+text = '''Invoice Total: $1,200.00  
+Please contact finance@example.com for queries.  
+More details at: https://company.com/invoice/123
+'''
+doc = nlp(text)
+emails = []
+urls = []
+numbers = []
+for token in doc:
+    if token.like_email:
+        emails.append(token.text)
+    elif token.like_url:
+        urls.append(token.text)
+    elif token.like_num:
+        numbers.append(token.text)
+
+
+print("Emails:", emails)
+print("URLs:", urls)
+print("Numbers:", numbers)

33753de0b4d8b0a4818fa4f77467408c28aa8986
Kaushik H <kaushikhari824@gmail.com>
2025-06-01 20:50:47 +0530

    updated

---
+++ nlp-foundations/01_tokenizer_features/token_analysis.py
@@ -0,0 +1,13 @@
+# everything about token attributes and their functions
+import spacy
+nlp = spacy.load("en_core_")
+doc = nlp('I have two bananas and i have one $.')
+for token in doc:
+    print(token)
+for token in doc:
+    print(token, '=>', 'index: ', token.i,
+          'is alpha: ', token.is_alpha,
+          'is punctuated: ', token.is_punct,
+          'is number: ', token.like_num,
+          'is currency: ', token.is_currency)
+    
